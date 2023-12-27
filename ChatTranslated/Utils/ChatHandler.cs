4beb0b771ad790680af25a317522190753d8101b
kelvin124124 <lindedi03@gmail.com>
2023-12-27 11:34:14 +0800

    something

--- ChatTranslated/Utils/ChatHandler.cs
+++ ChatTranslated/Utils/ChatHandler.cs
@@ -1,6 +1,24 @@
+using Dalamud.Game.Text;
+using Dalamud.Game.Text.SeStringHandling;
+using Dalamud.Plugin.Services;
+using Lumina.Excel.GeneratedSheets;
+using System;
+
 namespace ChatTranslated.Utils
 {
     internal class ChatHandler
     {
+        public ChatHandler()
+        {
+            Service.chatGui.ChatMessage += OnChatMessage;
+        }
+
+        private void OnChatMessage(XivChatType type, uint senderId, ref SeString sender, ref SeString message, ref bool isHandled)
+        {
+            if (10 <= (uint)type && (uint)type <= 15) 
+            {
+                
+            }
+        }
     }
 }
