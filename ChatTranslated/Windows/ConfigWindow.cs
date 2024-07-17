117611410dc71d0c4baccca180ce316e5c534b4f
kelvin124124 <lindedi03@gmail.com>
2024-07-17 21:10:48 +0800

    Fix secret reading

--- ChatTranslated/Windows/ConfigWindow.cs
+++ ChatTranslated/Windows/ConfigWindow.cs
@@ -62,7 +62,7 @@ public class ConfigWindow : Window
         "Chat Translated config window",
         ImGuiWindowFlags.AlwaysAutoResize)
     {
-        Size = new Vector2(800, 340);
+        Size = new Vector2(600, 340);
     }
 
     private static string DeepLApiKeyInput = Service.configuration.DeepL_API_Key;
@@ -400,7 +400,7 @@ public class ConfigWindow : Window
         switch (configuration.SelectedTranslationEngine)
         {
             case TranslationEngine.DeepL:
-                ImGui.TextUnformatted(Resources.DeepLExplanation);
+                ImGui.TextWrapped(Resources.DeepLExplanation);
                 ImGui.Separator();
                 DrawDeepLSettings(configuration);
                 break;
